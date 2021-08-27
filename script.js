d29ccb9ff6c2f8e10834252043798df2cda24d88
antor <antorinfo@gmail.com>
2021-08-27 16:03:59 +0600

    fix bug

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const msg = document.querySelector('#message');
 
 document.querySelector('form').addEventListener('submit', function (e) {
   e.preventDefault();
+  if (msg.value === '') return;
   output.textContent = msg.value;
   msg.value = '';
 });
